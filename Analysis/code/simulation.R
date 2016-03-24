0451afbf40bf7f95d46c5692b1d1ed09fa1d9aa5
Boying Gong <jorothy_gong@berkeley.edu>
2016-03-24 13:28:47 -0700

    add AR1, AR2, MA1 simulation

--- Analysis/code/simulation.R
+++ Analysis/code/simulation.R
@@ -1,42 +1,18 @@
 
+library(ggplot2)
+library(cowplot)
+
 source("risk_diagostics_function.R")
 
 ts.param = seq(from = -0.9, to = 0.9, by = 0.05)
 ts.param = ts.param[ts.param != 0]
 
-# # AR(1)
-# 
-# ar.sim = lapply(ts.param, function(x){
-#   arima.sim(model=list(ar=x), n=1000, sd = 0.01)
-# })
-# 
-# names(ar.sim) = ts.param
-# 
-# for (asset in names(ar.sim)){
-#   ar.sim[[asset]] = as.xts(ar.sim[[asset]])
-# }
-# 
-# # Risk diagnostic
-# 
-# risk_df <- function(Data, FUN, ...){
-#   res = rep(0, length(Data))
-#   names(res) = names(Data)
-#   for (i in names(Data)){
-#     res[i] <- do.call(FUN, list(Data[[i]], ...))
-#   }
-#   return(res)
-# }
-# 
-# ES.ar.sim.0.95 = risk_df(ar.sim, FUN = 'ES', p = 0.95)
-# VaR.ar.sim.0.95 =  risk_df(ar.sim, FUN = 'VaR', p = 0.95)
-# volatility.ar.sim = risk_df(ar.sim, FUN = 'volatility')
-# maxDrawdown.ar.sim = risk_df(ar.sim, FUN = 'maxDrawdown')
+#############
+## example ##
+#############
 
-
-
-
-sim_ts <- function(x, rep = 1000, p = 0.95, sd = 0.0001){
-  ar.sim = arima.sim(model=list(ar=x), n=rep)
+sim_ts <- function(p = 0.95, model, n, rand.gen){
+  ar.sim = arima.sim(model = model, n = n, rand.gen = rand.gen)
   ar.sim = as.xts(ar.sim)
   res = c(ES(ar.sim, p = p),
           VaR(ar.sim, p = p),
@@ -46,8 +22,8 @@ sim_ts <- function(x, rep = 1000, p = 0.95, sd = 0.0001){
   return(res)
 }
 
-rep_sim_ts <- function(x, rep = 1000, p = 0.95){
-  df = replicate(rep, sim_ts(x))
+rep_sim_ts <- function(rep = 1000, p = 0.95, model, n, rand.gen){
+  df = replicate(rep, sim_ts(p = 0.95, model = model, n = n, rand.gen = rand.gen))
   sim.maxDrawdown = df['maxDrawdown', ] 
   res = c(mean( df['ES', ] ),
           mean( df['VaR', ] ),
@@ -57,17 +33,26 @@ rep_sim_ts <- function(x, rep = 1000, p = 0.95){
   return(res)
 }
 
+######################
+## function example ##
+######################
+
+# x=0.2
+# test_ts = sim_ts(p = 0.95, model=list(ar=x), n=1000, 
+#                  rand.gen = function(n) rnorm(n, sd = 0.01))
+# rep_sim_ts(model=list(ar=x), n=1000, 
+#            rand.gen = function(n) rnorm(n, sd = 0.01))
+
 ### plot(density(sim.maxDrawdown))
 
-sim.risk.ar.1 = sim.risk
-sim.risk.ar.1 = sapply(ts.param, function(x){rep_sim_ts(x)})
+## AR(1) ##
+###########
+
+sim.risk.ar.1 = sapply(ts.param, 
+                       function(x){rep_sim_ts(model=list(ar=x), n=1000, 
+                                              rand.gen = function(n) rnorm(n, sd = 0.01))})
 sim.risk.ar.1 = data.frame(cbind(t(sim.risk.ar.1), ts.param))
-rownames(sim.risk.ar.1) = ts.param
 
-plot(ts.param,sim.risk.ar.1['CED',])
-
-library(ggplot2)
-library(cowplot)
 plot_grid(ggplot(sim.risk.ar.1, aes(x = ts.param, y = ES)) + geom_point() + theme_bw() + 
             xlab("AR(1) Coefficient"),
           ggplot(sim.risk.ar.1, aes(x = ts.param, y = VaR)) + geom_point() + theme_bw() + 
@@ -78,22 +63,61 @@ plot_grid(ggplot(sim.risk.ar.1, aes(x = ts.param, y = ES)) + geom_point() + them
             xlab("AR(1) Coefficient"),
           ncol = 2, align = 'v')
 
+## AR(2) ##
+###########
 
+ts.param.2 = expand.grid(seq(from = -0.9, to = 0.9, by = 0.1), 
+                         seq(from = -0.9, to = 0.9, by = 0.1))
+ts.param.2 = ts.param.2[ts.param.2[, 1] != 0, ]
+ts.param.2 = ts.param.2[ts.param.2[, 2] != 0, ]
+ts.param.2 = ts.param.2[-which(abs(abs(ts.param.2[, 1]) + abs(ts.param.2[, 2]) - 1)<=1e-4), ]
+rownames(ts.param.2) = NULL
 
+# check stationary
+roots.ts.param.2 = t(apply(ts.param.2, 1,
+                           function(x) polyroot(c(-rev(x), 1))))
+ts.param.2 = ts.param.2[-which(rowSums(abs(roots.ts.param.2) > 1) != 0), ]
+rownames(ts.param.2) = NULL
+# calculate the autocorrelation
+sim.ar.2.acf = t(apply(ts.param.2, 1,
+                 function(x) ARMAacf(ar = x, lag.max = 3)[2:4]))
+# simulation
+sim.risk.ar.2 = apply(ts.param.2, 1, 
+                       function(x){rep_sim_ts(model=list(ar=x), n=1000, 
+                                              rand.gen = function(n) rnorm(n, sd = 0.01))})
+sim.risk.ar.2 = data.frame(cbind(t(sim.risk.ar.2), ar.2.acf))
 
+## MA(1) ##
+###########
 
+sim.risk.ma.1 = sapply(ts.param, 
+                       function(x){rep_sim_ts(model=list(ma=x), n=1000, 
+                                              rand.gen = function(n) rnorm(n, sd = 0.01))})
+sim.risk.ma.1 = data.frame(cbind(t(sim.risk.ma.1), ts.param))
+rownames(sim.risk.ma.1) = ts.param
 
+plot_grid(ggplot(sim.risk.ma.1, aes(x = ts.param, y = ES)) + geom_point() + theme_bw() + 
+            xlab("MA(1) Coefficient"),
+          ggplot(sim.risk.ma.1, aes(x = ts.param, y = VaR)) + geom_point() + theme_bw() + 
+            xlab("MA(1) Coefficient"),
+          ggplot(sim.risk.ma.1, aes(x = ts.param, y = sd)) + geom_point() + theme_bw() + 
+            xlab("MA(1) Coefficient"),
+          ggplot(sim.risk.ma.1, aes(x = ts.param, y = CED)) + geom_point() + theme_bw() + 
+            xlab("MA(1) Coefficient"),
+          ncol = 2, align = 'v')
 
-
-
+##################
+## rolling stat ##
+##################
 
 # for (asset in names(ar.sim)){
 #   ar.sim[[asset]] = data.frame(Date = rownnames(as.xts(ar.sim[[asset]])),
 #                                retrn_dl = as.vector(ar.sim[[asset]]))
 # }
 
-
-# normality test
+##################
+# normality test #
+##################
 
 library(Matching)
 
