abb66ea08dcd8282e6de127c4b4f6668478d2832
Boying Gong <jorothy_gong@berkeley.edu>
2016-04-22 15:41:21 -0700

    update risk concentration

--- Analysis/code/risk_concentration.R
+++ Analysis/code/risk_concentration.R
@@ -3,16 +3,16 @@ source("risk_diagostics_function.R")
 
 library(PerformanceAnalytics)
 
-max_drawdown = function(returns){
-  log_returns = log(returns + 1)
+max_drawdown = function(r){
+  log_r = log(r + 1)
   sum = 0
   this_sum = 0
   Ri = 1
   Rj = 1
   this_Ri = 1
   this_Rj = 1
-  for (i in 1:length(returns)){
-    this_sum = this_sum + log_returns[i]
+  for (i in 1:length(r)){
+    this_sum = this_sum + log_r[i]
     this_Rj = i
     if (this_sum<sum){
       sum = this_sum
@@ -27,8 +27,47 @@ max_drawdown = function(returns){
   return(list(max_drawdown=max_drawdown, Ri=Ri, Rj=Rj))
 }
 
+## r: data frame or matrix, each column represents the return series of one asset class
+## weight: length equal to the number of columns in r, 
+## represents the weight of each assets in the portfolio
+## return: maximum drawdown, a vector of drawdown contributions(length = number of asset class)
 
-a1 = 23
+### CHECK CORRECTNESS!!!!!!!
+
+drawdown_contribution = function(r, weight){
+  if (!all.equal(sum(weight), 1)) stop("weight must sum to one")
+  r_all = r%*%weight
+  max_d = max_drawdown(r_all)
+  contribution = (1-apply(r[maxD$Ri:max_d$Rj, ]+1, 2, prod)) * weight
+  contribution = max_d_asset/sum(max_d_asset)
+  return(list(max_drawdown = max_d$max_drawdown, contribution = contribution))
+}
+
+drawdown_contribution(r, c(0.4, 0.6))
+
+## calculate risk contribution (rolling and )
+
+risk_contribution = function(){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+r = matrix(returns, nc =2)
+weight = c(0.4, 0.6)
+r%*%weight
+
+
+a1 = 1
 a2 = 100
 
 dt <- as.data.frame(assetData$AGG$retrn_dl[a1:a2])
